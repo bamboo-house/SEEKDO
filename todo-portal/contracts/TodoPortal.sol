7fe03fb6faf03ed32d4454583bef0d1f6b680ead
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-09-03 00:50:48 +0900

    コントラクトに関数追加

--- todo-portal/contracts/TodoPortal.sol
+++ todo-portal/contracts/TodoPortal.sol
@@ -6,7 +6,25 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract TodoPortal {
+
+  uint256 totalTodo;
+
   constructor() {
     console.log("Here is my fiirst smart contract!");
   }
+
+  function createTodo() public {
+    totalTodo += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function deleteTodo() public {
+
+  }
+
+  function getTotalTodo() public view returns(uint256) {
+    console.log("We have %d total waves!", totalTodo);
+    return totalTodo;
+  }
+
 }
