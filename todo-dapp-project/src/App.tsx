0eaa241cccb45eec172dbe27de7878e5a92ab5dd
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-09-07 07:13:42 +0900

    フロントのテスト

--- todo-dapp-project/src/App.tsx
+++ todo-dapp-project/src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const checkIFWalletIsConnected = () => {
+    // window.ethereumにアクセスできることを確認する
+    const { ethereum }: any = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
+
+  useEffect(() => {
+    checkIFWalletIsConnected();
+  }, []);
   return (
-    <div>Hello!</div>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+          <span role="img" aria-label="hand-wave">
+            👋
+          </span>{" "}
+          WELCOME!
+        </div>
+        <div className="bio">
+          イーサリアムウォレットを接続して、「
+          <span role="img" aria-label="hand-wave">
+            👋
+          </span>
+          (wave)」を送ってください
+          <span role="img" aria-label="shine">
+            ✨
+          </span>
+        </div>
+        <button className="waveButton" onClick={() => {}}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
   );
 }
 
