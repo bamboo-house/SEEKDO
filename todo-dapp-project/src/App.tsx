401a5885ac4355cfb61ff46349e1c40bc530f181
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-09-10 22:52:47 +0900

    ユーザーアカウントにアクセスできるか確認

--- todo-dapp-project/src/App.tsx
+++ todo-dapp-project/src/App.tsx
@@ -1,41 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 import { ethers } from "ethers";
 
 const App = () => {
-  const checkIFWalletIsConnected = () => {
-    // window.ethereumにアクセスできることを確認する
-    const { ethereum }: any = window;
-    if (!ethereum) {
-      console.log("Make sure you have MetaMask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
-    }
-  };
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
 
-  const createTodo = async () => {
+  const checkIFWalletIsConnected = async () => {
+    // window.ethereumにアクセスできることを確認する
     try {
-      // ユーザーがMetaMaskを持っているか確認
       const { ethereum }: any = window;
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const myTodoPortalContract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        );
-        let count = await myTodoPortalContract.createTodo();
-        console.log("Retrieved total wave count...", count.toNumber());
-        console.log("Signer:", signer);
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
       } else {
-        console.log("Ethereum object doesn't exist!");
+        console.log("We have the ethereum object", ethereum);
+      }
+  
+      // ユーザーのウォレットへのアクセスが許可されているかどうかを確認
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
       }
     } catch (error) {
       console.log(error);
     }
   };
+
+  // const createTodo = async () => {
+  //   try {
+  //     // ユーザーがMetaMaskを持っているか確認
+  //     const { ethereum }: any = window;
+  //     if (ethereum) {
+  //       const provider = new ethers.providers.Web3Provider(ethereum);
+  //       const signer = provider.getSigner();
+  //       const myTodoPortalContract = new ethers.Contract(
+  //         contractAddress,
+  //         contractABI,
+  //         signer
+  //       );
+  //       let count = await myTodoPortalContract.createTodo();
+  //       console.log("Retrieved total wave count...", count.toNumber());
+  //       console.log("Signer:", signer);
+  //     } else {
+  //       console.log("Ethereum object doesn't exist!");
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   useEffect(() => {
     checkIFWalletIsConnected();
