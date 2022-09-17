4a2c9475f2fecc3cf402c320a2d25d2a7828318a
bamboo-house <takeshu001@gmail.com>
2022-09-17 21:55:09 +0900

    Cardコンポーネントを追加

--- todo-dapp-project/src/components/TodoList.tsx
+++ todo-dapp-project/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { dummyData } from "./DummyData";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
 
 export const TodoList = () => {
   const [todoItems, setTodoItems] = useState(dummyData);
@@ -10,13 +11,25 @@ export const TodoList = () => {
   }, []);
 
   return (
-    <div>
-      {todoItems.map((item) => (
-        <ul>
-          <li>{item.creator}</li>
-          <li>{item.text}</li>
-        </ul>
-      ))}
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          なんか
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          やらないといけないことです。やらないといけないことです。
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="success">Share</Button>
+        <Button size="small" color="success">Learn More</Button>
+      </CardActions>
+  </Card>
+      // {/* {todoItems.map((item) => (
+      //   <ul>
+      //     <li>{item.creator}</li>
+      //     <li>{item.text}</li>
+      //   </ul>
+      // ))} */}
   );
 };
