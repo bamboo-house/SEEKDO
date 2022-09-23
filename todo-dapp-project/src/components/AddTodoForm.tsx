284c99d0537a3a013369f3bbf64c3428570c62e4
bamboo-house <takeshu001@gmail.com>
2022-09-24 00:09:39 +0900

    細かいレイアウト調整

--- todo-dapp-project/src/components/AddTodoForm.tsx
+++ todo-dapp-project/src/components/AddTodoForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form'
 import { styled } from '@mui/material/styles';
 import AddIcon from '@mui/icons-material/Add';
-import { Typography } from '@mui/material';
+import { Typography, Box, TextField } from '@mui/material';
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
 import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
@@ -10,10 +10,11 @@ import MuiAccordionDetails from '@mui/material/AccordionDetails';
 
 const Accordion = styled((props: AccordionProps) => (<MuiAccordion disableGutters elevation={0} {...props} />))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
+  borderRadius: 4,
+  marginBottom: 20,
   '&:before': {
     display: 'none',
   },
-  marginBottom: 20,
 }));
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
@@ -25,7 +26,7 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
   flexDirection: 'row-reverse',
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
     transform: 'rotate(90deg)',
-    color: '#002984',
+    color: `${theme.palette.primary.main}`,
   },
   '& .MuiAccordionSummary-content': {
     marginLeft: theme.spacing(1),
@@ -33,8 +34,6 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
 export const AddTodoForm = () => {
@@ -49,12 +48,18 @@ export const AddTodoForm = () => {
   
   return (
     <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Todoを追加する</Typography>
-        </AccordionSummary>
+        <Box sx={{ display: "flex", justifyContent: 'center' }}>
+          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+            <Typography>Todoを追加</Typography>
+          </AccordionSummary>
+        </Box>
         <AccordionDetails>
           <Typography>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+              <TextField required label="Todo名" variant="outlined" size="small" margin="normal"/>
+              <TextField multiline rows={4} label="詳細" variant="outlined" size="small" margin="normal"/>
+            </Box>
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
               <div>
                 <label>Title</label>
                 <input id="title" {...register('title')}/>
@@ -64,7 +69,7 @@ export const AddTodoForm = () => {
                 <input id="body" {...register('body')}/>
               </div>
               <button type="submit">保存</button>
-            </form>
+            </form> */}
           </Typography>
         </AccordionDetails>
       </Accordion>
