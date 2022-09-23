6a4a487828cbb457db880af707ccd92428d3215a
bamboo-house <takeshu001@gmail.com>
2022-09-23 20:18:46 +0900

    ヘッダーを修正

--- todo-dapp-project/src/components/Header.tsx
+++ todo-dapp-project/src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import HdrStrongIcon from '@mui/icons-material/HdrStrong';
 
 export const Header = () => {
 
   return (
     <AppBar position="static" sx={{ mb: 3 }}>
       <Toolbar>
-        <Typography variant="h4" color="white" >SeekDo</Typography>
+        <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}><HdrStrongIcon/></IconButton>
+        <Typography variant="h6" color="white" >SEEKDO</Typography>
       </Toolbar>
     </AppBar>
   );
