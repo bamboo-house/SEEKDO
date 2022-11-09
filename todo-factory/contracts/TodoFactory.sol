e798b0a5b8c2db21de2b73cc815604d047a7bb9a
bamboo-house <takeshu001@gmail.com>
2022-11-09 19:42:15 +0900

    データモデリング勉強中

--- todo-factory/contracts/TodoFactory.sol
+++ todo-factory/contracts/TodoFactory.sol
@@ -9,10 +9,9 @@ contract TodoFactory {
 
   event  NewTodo(uint id, address, string title, string body, uint256 poolAmount, uint32 deadline);
 
-  // 竹内：アドレスとプール金額を確認
   mapping(address => uint) balance;
+  // 処理速度向上のために定義
   mapping(address => uint) todoCountByOwner;
-
   struct Todo {
     uint id;
     address owner;
@@ -42,7 +41,6 @@ contract TodoFactory {
     return balance[msg.sender];
   }
 
-  // 10/5 viewをつけたときなぜか、run.tsでエラーが出る
   function createTodo(string memory _title, string memory _body, uint256 _poolAmount, uint32 _deadline) public {
     console.log("%s create todo w/ with \ntitle: %s", msg.sender, _title);
     console.log("body: %s", _body);
@@ -51,7 +49,7 @@ contract TodoFactory {
 
     // 竹内: _deadlineを正しい型に変換？
 
-    // 受け取ったデータをブロックチェーン上に保存する
+    // 受け取ったTodoをブロックチェーン上に保存する
     uint id = todos.length + 1;
     todos.push(Todo(id, msg.sender, _title, _body, _poolAmount, _deadline, false, uint32(block.timestamp)));
     todoCountByOwner[msg.sender]++;
@@ -65,14 +63,15 @@ contract TodoFactory {
     balance[msg.sender] += msg.value;
   }
 
-  function withdrow(uint _amount) public {
+  function _withdrow(uint _amount) private {
     balance[msg.sender] -= _amount;
     payable(msg.sender).transfer(_amount);
   }
 
   // 竹内：完了ボタンを押したときに返金処理とdoneをtrueにする。
-  function doneTodo() public view {
+  function doneTodo(uint id, uint _amount) public {
     console.log("%s done todo!", msg.sender);
+    _withdrow(_amount);
     // isDoneがfalseの場合下記実行
     // deadlineが今の時刻を超えている場合下記実行
     // isDoneをtrueにする
