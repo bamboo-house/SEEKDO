39602b6db9fc6b6c06f600d05e34a03f9c93bb35
bamboo-house <takeshu001@gmail.com>
2022-11-10 19:23:20 +0900

    doneTodo関数実装

--- todo-factory/contracts/TodoFactory.sol
+++ todo-factory/contracts/TodoFactory.sol
@@ -7,75 +7,79 @@ import "hardhat/console.sol";
 
 contract TodoFactory {
 
-  event  NewTodo(uint id, address, string title, string body, uint256 poolAmount, uint32 deadline);
-
-  mapping(address => uint) balance;
-  // 処理速度向上のために定義
-  mapping(address => uint) todoCountByOwner;
   struct Todo {
     uint id;
     address owner;
     string title;
     string body;
-    uint256 poolAmount;
+    uint256 amount;
     uint32 deadline;
     bool isDone;
-    uint32 timestamp;
+    // uint32 timestamp;
   }
 
   Todo[] todos;
+  // OwnerとTodoは「1対多」の関係なので、OwnerにTodoの情報をもたすために記述
+  mapping(address => uint[]) ownerTodoIds;
+  mapping(address => uint) balance;
 
+  /*
+  *  Todo処理
+  */ 
   function getAllTodos() public view returns(Todo[] memory) {
-    Todo[] memory result = new Todo[](todoCountByOwner[msg.sender]);
+    return todos;
+  }
+
+  function getOwnerTodos() public view returns(Todo[] memory) {
+    uint[] memory todoIds = ownerTodoIds[msg.sender];
+    Todo[] memory result = new Todo[](todoIds.length);
     uint counter = 0;
-    for (uint i = 0; i < todos.length; i++) {
-        if (todos[i].owner == msg.sender) {
-            result[counter] = todos[i];
-            counter += 1;
-        }
+    for (uint i = 0; i < todoIds.length; i++) {
+      if (todoIds[i] != 0) {
+        uint todoId = todoIds[i];
+        result[i] = (todos[todoId - 1]);
+        counter++;
+      }
     }
     return result;
   }
 
+  function createTodo(string memory _title, string memory _body, uint _amount, uint32 _deadline) public {
+    uint id = todos.length + 1;
+    todos.push(Todo(id, msg.sender, _title, _body, _amount, _deadline, false));
+    ownerTodoIds[msg.sender].push(id);
+  }
+
+  function doneTodo(uint _id) public {
+    require(todos[_id - 1].owner == msg.sender, "Do not have this Todo");
+    require(todos[_id - 1].isDone == false, "Already done");
+    // 竹内： deadline以下ならOK
+    // require(todos[_id - 1].deadline <= timestamp);
+    _withdraw(todos[_id - 1].amount);
+    todos[_id - 1].isDone = true;
+  }
+
+
+  /*
+  *  プール処理
+  */
   function getBalance() public view returns(uint) {
     return balance[msg.sender];
   }
 
-  function createTodo(string memory _title, string memory _body, uint256 _poolAmount, uint32 _deadline) public {
-    console.log("%s create todo w/ with \ntitle: %s", msg.sender, _title);
-    console.log("body: %s", _body);
-    console.log("poolAmount: %s", _poolAmount);
-    console.log("deadline: %s", _deadline);
-
-    // 竹内: _deadlineを正しい型に変換？
-
-    // 受け取ったTodoをブロックチェーン上に保存する
-    uint id = todos.length + 1;
-    todos.push(Todo(id, msg.sender, _title, _body, _poolAmount, _deadline, false, uint32(block.timestamp)));
-    todoCountByOwner[msg.sender]++;
-
-    // 新しいTdodoを作ったことをフロントに伝える
-    emit NewTodo(id, msg.sender, _title, _body, _poolAmount, _deadline);
-  }
-
-  // 竹内：プール処理
   function deposit() public payable {
     balance[msg.sender] += msg.value;
   }
 
-  function _withdrow(uint _amount) private {
+  /*
+  *  internal
+  */
+  function _withdraw(uint _amount) internal {
+    require(balance[msg.sender] >= _amount, "Insufficient balance");
+    uint beforeWithdraw = balance[msg.sender]; 
     balance[msg.sender] -= _amount;
     payable(msg.sender).transfer(_amount);
+    uint afterWithdraw = balance[msg.sender];
+    assert(afterWithdraw == beforeWithdraw - _amount);
   }
-
-  // 竹内：完了ボタンを押したときに返金処理とdoneをtrueにする。
-  function doneTodo(uint id, uint _amount) public {
-    console.log("%s done todo!", msg.sender);
-    _withdrow(_amount);
-    // isDoneがfalseの場合下記実行
-    // deadlineが今の時刻を超えている場合下記実行
-    // isDoneをtrueにする
-    // amountの金額をwithdrowする
-  }
-
 }
